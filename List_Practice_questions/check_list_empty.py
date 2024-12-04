6e246354bdb051773ae551ef796b53218d16caf5
Sharuf <ali@gmail.com>
2024-12-04 20:41:02 +0530

    list isempty or not

---
+++ List_Practice_questions/check_list_empty.py
@@ -0,0 +1,10 @@
+def solve():
+    # user_list = input("Enter a list item by commma seperated :")
+    # ls = user_list.split(",")
+    ls = []
+    # print(ls)
+    if len(ls) >= 1 :
+        print("list is not empty")
+    else :
+        print("list is empty")
+solve()

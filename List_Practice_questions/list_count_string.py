36cd6ebd39d8b0551fe83a0e73cfa3323fd53f29
Sharuf <ali@gmail.com>
2024-12-04 19:44:03 +0530

    count string

---
+++ List_Practice_questions/list_count_string.py
@@ -0,0 +1,11 @@
+def count():
+    # user_input = input("Enter list item as mix of string and number seperatated by commas :")
+    # use_list = user_input.split(",")
+    user_list = ["ali","b",1,2,4,"jack","j"]
+    count = 0
+    for v in user_list:
+        if type(v) is not int :
+            count+=1
+        # print(type(v))
+    print(count)
+count()
